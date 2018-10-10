b2fa6c9e50477cef90cc4e5a3f2d4ad9af5c63c7
mic615 <mic615@gmail.com>
2018-10-10 08:29:05 -0500

    initial push of .net conversion integrated with full calendar front end

---
+++ productive/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace productive.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public JsonResult GetTasks()
+        {
+            using (TasksEntities dc = new TasksEntities())
+            {
+                var tasks = dc.Tasks.ToList();
+                return new JsonResult { Data = tasks, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+
+        }
+    }
+}
