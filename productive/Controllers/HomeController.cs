c11e442ccf78a7071320b6a641f056279c3742ea
mic615 <mic615@gmail.com>
2018-10-12 03:05:27 -0500

    Added crud functionality

--- productive/Controllers/HomeController.cs
+++ productive/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace productive.Controllers
         {
             return View();
         }
-        
+
         public JsonResult GetTasks()
         {
             using (TasksEntities dc = new TasksEntities())
@@ -22,6 +22,80 @@ namespace productive.Controllers
                 return new JsonResult { Data = tasks, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
 
+        }
+
+        [HttpPost]
+        public JsonResult SaveTask(Task task)
+        {
+            var success = false;
+            using (TasksEntities dc = new TasksEntities())
+            {
+                if (task.EventID > 0)
+                {
+                    //update all values in a task multipurpose for all save functions specify active or complete on js side 
+                    var newTask = dc.Tasks.Where(a => a.EventID == task.EventID).FirstOrDefault();
+                    if (newTask != null)
+                    {
+                        newTask.Name = task.Name;
+                        newTask.startDate = task.startDate;
+                        newTask.endDate = task.endDate;
+                        newTask.Description = task.Description;
+                        newTask.Category = task.Category;
+                        newTask.assigned = task.assigned;
+                        newTask.completed = task.completed;
+                        newTask.ThemeColor = task.ThemeColor;
+
+                    }
+
+                }
+                else
+                {
+                    Task newTask1= new Task();
+                    DateTime? dateOrNull = task.startDate;
+                  
+                    if (dateOrNull != null)
+                    {
+                         task.startDate= dateOrNull.Value;
+                    }
+                    DateTime? dateOrNull2= task.endDate;
+
+                    if (dateOrNull2!= null)
+                    {
+                        task.endDate = dateOrNull2.Value;
+                    }
+                    newTask1.Name = task.Name;
+                    newTask1.startDate = dateOrNull.Value;
+                    newTask1.endDate = dateOrNull2.Value;
+                    newTask1.Description = task.Description;
+                    newTask1.Category = task.Category;
+                    newTask1.assigned = task.assigned;
+                    newTask1.createdDate = DateTime.Now;
+                    newTask1.completed = task.completed;
+                    newTask1.ThemeColor = task.ThemeColor;
+                    dc.Tasks.Add(newTask1);
+                }
+                dc.SaveChanges();
+                success = true;
+            }
+            return new JsonResult { Data = new { status = success } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteTask(int eventID)
+        {
+            var success = false;
+            using (TasksEntities dc = new TasksEntities())
+            {
+                var oldTask = dc.Tasks.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (oldTask != null)
+                {
+                    dc.Tasks.Remove(oldTask);
+                    dc.SaveChanges();
+                    success = true;
+
+                }
+                return new JsonResult { Data = new { status = success } };
+            }
         }
     }
 }
